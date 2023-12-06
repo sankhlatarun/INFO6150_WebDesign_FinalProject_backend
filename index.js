4cc486b1afc108893bd080e493e2b2b850cbd901
sankhlatarun <sankhla.t@northeastern.edu>
2023-12-06 15:51:25 -0500

    comment

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+	console.log(`Server running on port : ${port}`);
 });
 
