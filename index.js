a0ffdbd54752aa174c01aa86cebcfd9c9ccff2ee
Yash Jain NUID(002871982) <145181114+yashjainNEU@users.noreply.github.com>
2023-12-07 11:52:32 -0500

    login api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const cors = require('cors');
 const flightRouter = require('./routes/flightRoutes');
+const {User} = require('./models/user.Model');
 
 const mongoUri = `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PASS}@cluster0.ucfrjku.mongodb.net/${process.env.DB_ADMIN}`;
 
@@ -27,6 +28,32 @@ app.use(cors());
 app.use('/flights',flightRouter);
 app.use('/hotels/',require('./routes/hotelRoutes'));
 
+
+app.post('/login', async (req,res)=>{
+	try{
+
+		  try{
+			const user = new ({
+			  name: req.body.name,
+			  type: req.body.type,
+			  email: req.body.email,
+			  email_verified: false,
+				nickname:  req.body.nickname,
+				picture: req.body.picture,
+				sub: req.body.sub
+			});
+		  
+			const data  = await User.save();
+			res.send({data});
+		  } catch(err){
+			res.status(500).json({ error: 'Internal Server Error',message:err.message });
+			console.log(err);
+		  }
+
+	}catch(err){
+
+	}
+})
 app.get("/", (req, res) => {
 	res.send("Welcome to TravelEasy");
 });
