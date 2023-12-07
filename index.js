0214dcdba540e10ccdb209d90fd7efdfbbbbb599
ShreyasKulkarniNU <kulkarni.shreyas@northeastern.edu>
2023-12-06 21:06:45 -0500

    Adding backend code for BusControllers and BusModels

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const cors = require('cors');
 const flightRouter = require('./routes/flightRoutes');
-
+const cityRouter = require("./controller/busController/city.controller");
+const busRouter = require("./controller/busController/bus.controller");
+const userRouter=require("./controller/busController/user.controller");
+const orderRouter=require("./controller/busController/order.controller")
+const paymentController = require('./controller/busController/payment.controller');
 const mongoUri = `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PASS}@cluster0.ucfrjku.mongodb.net/${process.env.DB_ADMIN}`;
 
 mongoose.connect(mongoUri, {
@@ -23,9 +27,14 @@ app.use(xss());
 app.use(mongoSanitize());
 
 app.use(cors());
-
+app.use("/user",userRouter)
+app.use("/city", cityRouter);
+app.use("/bus", busRouter);
+app.use("/order", orderRouter);
 app.use('/flights',flightRouter);
 app.use('/hotels/',require('./routes/hotelRoutes'));
+
+app.use("/api/payment", paymentController);
 
 app.get("/", (req, res) => {
 	res.send("Welcome to TravelEasy");
