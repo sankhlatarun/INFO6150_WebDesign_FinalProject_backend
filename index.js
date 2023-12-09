8b32ef8a5bd4208045402d6d8801cdf526003015
sankhlatarun <sankhla.t@northeastern.edu>
2023-12-09 05:04:50 -0500

    stripe payment integration

--- index.js
+++ index.js
@@ -6,23 +6,24 @@ const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const cors = require('cors');
 const flightRouter = require('./routes/flightRoutes');
-const {User} = require('./models/user.Model');
+const { User } = require('./models/user.Model');
 const cityRouter = require("./controller/busController/city.controller");
 const busRouter = require("./controller/busController/bus.controller");
-const userRouter=require("./controller/busController/user.controller");
-const orderRouter=require("./controller/busController/order.controller")
+const userRouter = require("./controller/busController/user.controller");
+const orderRouter = require("./controller/busController/order.controller")
 const paymentController = require('./controller/busController/payment.controller');
 const mongoUri = `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PASS}@cluster0.ucfrjku.mongodb.net/${process.env.DB_ADMIN}`;
+const stripe = require('stripe')(process.env.DB_STRIPE_KEY);
 
 mongoose.connect(mongoUri, {
-useNewUrlParser:true,
-useUnifiedTopology:true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 })
 .then(() => {
 	console.log("connected")
 })
 .catch((err) => {
-    console.error("Error connecting to MongoDB:", err.message);
+	console.error("Error connecting to MongoDB:", err.message);
 });
 app.use(express.json());
 
@@ -30,44 +31,64 @@ app.use(xss());
 app.use(mongoSanitize());
 
 app.use(cors());
-app.use("/user",userRouter)
+app.use("/user", userRouter)
 app.use("/city", cityRouter);
 app.use("/bus", busRouter);
 app.use("/order", orderRouter);
 app.use("/api/payment", paymentController);
-app.use('/flights',flightRouter);
-app.use('/hotels/',require('./routes/hotelRoutes'));
+app.use('/flights', flightRouter);
+app.use('/hotels/', require('./routes/hotelRoutes'));
 
 app.use("/api/payment", paymentController);
+
+app.get("/secret", async (req, res) => {
+	try {
+
+		if (!req.body.amount) return res.status(400).send('Amount is required');
+		const stripe = require('stripe')(process.env.DB_STRIPE_KEY);
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount: req.body.amount,
+			currency: 'usd',
+			automatic_payment_methods: {
+				enabled: true,
+			},
+		});
+		res.send({ client_secret: paymentIntent.client_secret });
+	}
+	catch (err) {
+		console.log(err)
+		res.status(500).json({ error: 'Internal Server Error', message: err.message });
+	}
+});
 
 app.get("/", (req, res) => {
 	res.send("Welcome to TravelEasy");
 });
 
-app.post("/createUser", async(req, res) => {
-	try{
+app.post("/createUser", async (req, res) => {
+	try {
 		const user = new User({
-		  name: req.body.name,
-		  password: req.body.password,
-		  avatar: req.body.avatar,
-		  role: req.body.role,
-		  address: req.body.address,
-		  city: req.body.city,
-		  state: req.body.state,		  
-		  email: req.body.email,
-		  state: req.body.state,
-		  country: req.body.country,
-		  zip: req.body.zip,
-		  phone: req.body.phone,
-		  bookedHotels: req.body.bookedHotels ? req.body.bookedHotels : [],
+			name: req.body.name,
+			password: req.body.password,
+			avatar: req.body.avatar,
+			role: req.body.role,
+			address: req.body.address,
+			city: req.body.city,
+			state: req.body.state,
+			email: req.body.email,
+			state: req.body.state,
+			country: req.body.country,
+			zip: req.body.zip,
+			phone: req.body.phone,
+			bookedHotels: req.body.bookedHotels ? req.body.bookedHotels : [],
 		});
-	  
-		const data  = await user.save();
-		res.send({data});
-	  } catch(err){
-		res.status(500).json({ error: 'Internal Server Error',message:err.message });
+
+		const data = await user.save();
+		res.send({ data });
+	} catch (err) {
+		res.status(500).json({ error: 'Internal Server Error', message: err.message });
 		console.log(err);
-	  }
+	}
 });
 
 const port = process.env.PORT || 3001;
